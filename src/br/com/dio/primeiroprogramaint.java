fef548bca411cf5d2f2bb01f3672d880356724f9
mokshano11 <mateusggruber@gmail.com>
2023-03-08 19:32:12 -0500

    second commit

--- src/br/com/dio/primeiroprogramaint.java
+++ src/br/com/dio/primeiroprogramaint.java
@@ -7,9 +7,49 @@ public class primeiroprogramaint {
         Gato gato = new Gato();
         System.out.println(gato);
 
+        Livro livro = new Livro ("o problema dos 3 corpos", 300);
+
         /*int a = 5;
         int b = 3;
         System.out.println("Hello World!");*/
     }
 }
+
+class Livro {
+    private String nome;
+    private Integer npag;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNpag() {
+        return npag;
+    }
+
+    public void setNpag(Integer npag) {
+        this.npag = npag;
+    }
+
+    public Livro(String nome, Integer npag) {
+        this.nome = nome;
+        this.npag = npag;
+
+
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", npag='" + npag + '\'' +
+                '}';
+    }
+}
+
+
 
